a29e89c6b0b6dbf553b607a72ec01b0039a89712
tblouir <tblouirschool@gmail.com>
2022-11-15 19:15:10 -0700

    Add grid display and styling to CSS, refactor flipper in waitForResults

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ let flipper = 0
 
 generationSelector.addEventListener('change', preparePokemon)
 
+// Step 1
 async function getGeneration() {
   try {
     const request = await fetch(`https://pokeapi.co/api/v2/generation/${generationSelector.value}`)
@@ -19,6 +20,7 @@ async function getGeneration() {
   }
 }
 
+//Step 2
 async function getPokemon(obj) {
   console.log(obj);
   obj.pokemon_species.forEach((pokemon) => {
@@ -38,6 +40,7 @@ async function getPokemon(obj) {
   return true
 }
 
+//Step 3
 async function preparePokemon() {
   container.textContent = ''
   pokemonArray = []
@@ -51,6 +54,7 @@ async function preparePokemon() {
   }
 }
 
+// Inside Step 4
 function sortPokemon(arr) {
   console.log('Sorting');
   arr.sort((a, b) => {
@@ -58,33 +62,45 @@ function sortPokemon(arr) {
   })
 }
 
+// Step 4
 function waitForResults() {
-  if (pokemonArray.length !== pokemonFetchLength) {
-    flipper++
-    console.log(`Flipper: ${flipper}`);
-    timeout = setTimeout(waitForResults, 250);
-    return false
-  } else if (flipper === 5) {
-    throw `Request timed out after ${flipper} tries.`
-  } else {
-    sortPokemon(pokemonArray)
-    displayPokemon(pokemonArray)
-    clearTimeout(timeout)
-    return true
+  try {    
+    if (pokemonArray.length !== pokemonFetchLength) {
+      flipper++
+      if (flipper > 5) {
+        throw `Request timed out after ${flipper} tries.`
+      }
+      console.log(`Flipper: ${flipper}`);
+      timeout = setTimeout(waitForResults, 250);
+      return false
+    } else {
+      sortPokemon(pokemonArray)
+      displayPokemon(pokemonArray)
+      clearTimeout(timeout)
+      return true
+    }
+  } catch (error) {
+    console.log(error);
   }
 }
 
+// Step 5
 function displayPokemon(arr) {
   arr.forEach((pokemon) => {
     const div = document.createElement('div')
+    const textDiv = document.createElement('div')
+    const imageDiv = document.createElement('div')
     const image = document.createElement('img')
     const imageURL = pokemon.sprites.front_default
   
-    div.textContent = pokemon.name[0].toUpperCase() + pokemon.name.substring(1)
+    textDiv.textContent = pokemon.name[0].toUpperCase() + pokemon.name.substring(1)
+    div.classList.add('pokemon')
     image.src = imageURL
   
+    imageDiv.appendChild(image)
+    div.appendChild(imageDiv)
+    div.appendChild(textDiv)
     container.appendChild(div)
-    container.appendChild(image)
   })
 }
 
